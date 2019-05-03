8bc4a012209ca613ad86127db1e4d6b17798c6fb
Ryan Chenkie <ryanchenkie@gmail.com>
2019-05-03 11:47:22 -0600

    updates

--- src/App.js
+++ src/App.js
@@ -1,39 +1,73 @@
 import React, { Component } from 'react';
 import './App.css';
+import logo from './logo.svg';
+const csaLogo = require('./csa-logo.png');
 
 class App extends Component {
   state = {
-    hasHighTemp: false
+    hasHighTemp: false,
+    name: '',
+    temperature: null
   };
 
-  component;
+  issTempComponent;
 
   componentDidMount() {
-    this.component.addEventListener('hiTemp', e =>
-      this.setState({ hasHighTemp: true })
+    this.issTempComponent.addEventListener('hiTemp', e =>
+      this.setState({
+        hasHighTemp: e.detail.alert,
+        name: e.name,
+        temperature: e.temperature
+      })
     );
   }
 
   componentWillUnmount() {
-    this.component.addEventListener('hiTemp');
+    this.issTempComponent.removeEventListener('hiTemp');
   }
 
   handleRef = component => {
-    this.component = component;
+    this.issTempComponent = component;
   };
 
   render() {
     return (
       <div className="App">
         {this.state.hasHighTemp && (
-          <div style={{ background: 'red', color: 'white' }}>High Temp!!!</div>
+          <div
+            style={{
+              background: 'red',
+              color: 'white',
+              padding: '30px',
+              fontSize: '40px',
+              fontWeight: 'bold'
+            }}
+          >
+            Ooops, High Temp Warning, sorry!
+          </div>
         )}
         <header className="App-header">
+          <div style={{ display: 'flex' }}>
+            <img
+              src={csaLogo}
+              style={{
+                height: '100px',
+                width: '100px',
+                padding: '15px'
+              }}
+            />
+            <h1>CSA Mission Control</h1>
+            <img
+              src={logo}
+              style={{
+                height: '140px',
+                width: '140px'
+              }}
+            />
+          </div>
           <iss-temperature-gauge
-            unit="fahrenheit"
+            unit="celsius"
             station-component="discombobulator"
-            width="300"
-            height="500"
             ref={this.handleRef}
           />
         </header>
